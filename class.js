bf3569743d50b98353d5185cd59e45d5e348fe03
Fah <piyavadeepariyavuth@gmail.com>
2019-03-25 14:46:37 -0400

    add class.js

---
+++ class.js
@@ -0,0 +1,72 @@
+// var cups = 1
+// if ( cups < 3) {
+//     console.log("Yes I'll take another cup of coffee")    
+// } else {
+//     console.log("I thinl I'm okay for now")
+// }
+
+
+// var temp = 90
+// var precipitation = false;
+// var indoor = true;
+// var rainning = false;
+// var snowing = false;
+// console.log(temp)
+// if(rainning === true || snowing === true){
+//     precipitation = true;
+// }
+// if(temp > 80 && precipitation === false){
+//     console.log("time to swim")
+// } else if(indoor = true){
+//     console.log("time to swim")
+// }
+
+// for(let bottles = 99; bottles >= 0; bottles--){
+//     if(bottles === 0){
+//         console.log("Hey! We need more root beer!");
+//     } else{
+//         console.log(bottles +" bottles of root beer on the wall");
+//     }
+// }
+
+// let fruits = "Apple";
+// switch (fruits) {
+ 
+// case "Banana":
+//     console.log("Hello")
+//     break;
+ 
+// case "Apple":
+//     console.log("Welcome")
+//     break;    
+// }
+// var x = 0;
+// for(x;x<=100; x++){
+    
+//     if(x%3===0 && x%5===0){
+//         console.log(x + ": what up");
+//     } else if(x%5===0){
+//         console.log(x + ": up");
+//     } else if(x%3===0){
+//         console.log(x + ": what");
+//     } else{
+//         console.log(x);
+//     }
+// }
+let myVar = 'c';
+switch(myVar){
+    case 'a':
+        console.log('Hey');
+        break;
+    case 'b':
+        console.log('Foo');
+        console.log('Bar');
+        break;
+    case 'c':
+        console.log('Bar');
+        break;
+} 
+
+
+
+
